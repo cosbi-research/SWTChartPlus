3bf6c55d70d5e0188856c68f9b5d8fd7ae829ecb
ross <ross@10.1.4.10>
2015-02-02 17:48:55 +0100

    First project push

---
+++ SWTChartPlus/src/org/swtchart/ext/Messages.java
@@ -0,0 +1,65 @@
+package org.swtchart.ext;
+
+/**
+ * Messages
+ */
+public class Messages {
+
+    /** the menu group for adjust axis range menus */
+    public static final String ADJUST_AXIS_RANGE_GROUP = "&Auto Range...";
+
+    /** the menu for adjust axis range */
+    public static final String ADJUST_AXIS_RANGE = "&Both Axes";
+
+    /** the menu for adjust X axis range */
+    public static final String ADJUST_X_AXIS_RANGE = "&X Axis";
+
+    /** the menu for adjust Y axis range */
+    public static final String ADJUST_Y_AXIS_RANGE = "&Y Axis";
+
+    /** the menu group for zoom in menus */
+    public static final String ZOOMIN_GROUP = "Zoom &In";
+
+    /** the menu for zoom in */
+    public static final String ZOOMIN = "Zoom &In\tCtrl+Plus";
+
+    /** the menu for zoom in X axis */
+    public static final String ZOOMIN_X = "Zoom In &X Axis";
+
+    /** the menu for zoom in Y axis */
+    public static final String ZOOMIN_Y = "Zoom In &Y Axis";
+
+    /** the menu group for zoom out menus */
+    public static final String ZOOMOUT_GROUP = "Zoom &Out";
+
+    /** the menu for zoom out */
+    public static final String ZOOMOUT = "Zoom &Out\tCtrl+Minus";
+
+    /** the menu for zoom out X axis */
+    public static final String ZOOMOUT_X = "Zoom Out &X Axis";
+
+    /** the menu for zoom out Y axis */
+    public static final String ZOOMOUT_Y = "Zoom Out &Y Axis";
+
+    /** the menu for save as */
+    public static final String SAVE_AS = "Save As...";
+
+    /** the menu for save as txt */
+    public static final String SAVE_AS_TXT = "txt";
+
+    /** the menu for save as png */
+    public static final String SAVE_AS_PNG = "png";
+
+    /** the menu for save as pdf */
+    public static final String SAVE_AS_PDF = "pdf";
+    
+    /** the menu for save as xls */
+    public static final String SAVE_AS_XLS = "xls";
+
+    /** the menu for opening properties dialog */
+    public static final String PROPERTIES = "&Properties...";
+
+    /** the title for save as dialog */
+	public static final String SAVE_AS_DIALOG_TITLE = "Save As";
+
+}
